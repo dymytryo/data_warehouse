393d214c96feb8909df1269c157be75da9f39228
Dmytro Valiaiev <91760255+dymytryo@users.noreply.github.com>
2025-07-05 19:28:14 -0500

    Update collect_stats.sql

--- snowflake/collect_stats.sql
+++ snowflake/collect_stats.sql
@@ -1,23 +1,20 @@
 -- macros/collect_all_sf_stats.sql
 
 {% macro collect_all_sf_stats() %}
-    {# Get tables from snowflake #}
-    {% set sf_tables = run_query(
-        "SELECT table_schema, table_name FROM snowflake.information_schema.tables WHERE table_schema LIKE '\\_%' ESCAPE '\\'"
-    ) %}
-    {# Get tables from snowflake_pii #}
-    {% set pii_tables = run_query(
-        "SELECT table_schema, table_name FROM snowflake_pii.information_schema.tables WHERE table_schema LIKE '\\_%' ESCAPE '\\'"
+    {%- set all_tables = run_query(
+        "
+        SELECT 'snowflake' AS catalog, table_schema, table_name
+        FROM snowflake.information_schema.tables
+        WHERE table_schema LIKE '\\_%' ESCAPE '\\'
+        UNION ALL
+        SELECT 'snowflake_pii' AS catalog, table_schema, table_name
+        FROM snowflake_pii.information_schema.tables
+        WHERE table_schema LIKE '\\_%' ESCAPE '\\'
+        "
     ) %}
 
-    {% for row in sf_tables %}
-        {% set table = "snowflake." ~ row['table_schema'] ~ '.' ~ row['table_name'] %}
-        {{ log("Collecting stats: " ~ table, info=True) }}
-        {% do run_query("ALTER TABLE " ~ table ~ " EXECUTE collect_statistics;") %}
-    {% endfor %}
-
-    {% for row in pii_tables %}
-        {% set table = "snowflake_pii." ~ row['table_schema'] ~ '.' ~ row['table_name'] %}
+    {% for row in all_tables %}
+        {% set table = row['catalog'] ~ '.' ~ row['table_schema'] ~ '.' ~ row['table_name'] %}
         {{ log("Collecting stats: " ~ table, info=True) }}
         {% do run_query("ALTER TABLE " ~ table ~ " EXECUTE collect_statistics;") %}
     {% endfor %}
